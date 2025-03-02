5669a8737ac4ec635ad3802a70baa0ed2d30f48f
Xario Zeekoei <u24637646@up.ac.za>
2025-03-02 13:10:28 +0200

    Add project files.

---
+++ xarioStudyGroup/Controllers/StudyGroupController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace xarioStudyGroup.Controllers
+{
+    public class StudyGroupController : Controller
+    {
+        // GET: StudyGroup
+        public ActionResult ListMembers()
+        {
+            //Creating the list of members for the StudyGroup
+            List<Models.MembersModel> members = new List<Models.MembersModel>();
+
+            //Adding all the study group members to the list labeled "members"
+            members.Add(new Models.MembersModel { StudentNumber = "u24637646", FirstName = "Xario", Surname = "Zeekoei", EmailAddress = "xario.zeekoei@tuks.co.za" });
+            members.Add(new Models.MembersModel { StudentNumber = "u01234567", FirstName = "Scott", Surname = "Allen", EmailAddress = "scott.allen@tuks.co.za" });
+            members.Add(new Models.MembersModel { StudentNumber = "u12345678", FirstName = "Kallie", Surname = "Kotze", EmailAddress = "kallie.kotze@tuks.co.za" });
+            members.Add(new Models.MembersModel { StudentNumber = "u23456789", FirstName = "Johan", Surname = "Smith", EmailAddress = "johan.smith@tuks.co.za" });
+            members.Add(new Models.MembersModel { StudentNumber = "u34567890", FirstName = "Jane", Surname = "Doe", EmailAddress = "jane.doe@tuks.co.za" });
+
+            //Passing the list of members to the view
+            return View(members);
+        }
+    }
+}
---
+++ xarioStudyGroup/Models/MembersModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace xarioStudyGroup.Models
+{
+    public class MembersModel
+    {
+        //Creating the Primary Key for the StudyGroupMembers Table
+        [Key]
+        [Display(Name = "Student Number")]
+        public string StudentNumber { get; set; }
+
+        //Creating the column for the StudyGroupMembers Table
+        [Display(Name = "Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Surname")]
+        public string Surname { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+
+    }
+}
